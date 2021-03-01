8229508b10cd96551e19f4b5deffd8fab481d9cf
b6320502517 <sarannut.l@ku.th>
2021-03-01 17:04:56 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char x[2][200] , y[2][200];
+	char x[200] , y[200];
 	int i,j,k=0;
 	scanf("%[^\n]s",x);
-	for(i=0;i<strlen(x);i++){
-		if(x[])
+	for(i=0;i<strlen(x)-1;i++){
+		if(x[i]!=x[i+1]){
+			y[k]==x[i];
+			k++;
+		}
 	}
+	printf("%s",y);
+	return 0;
 }
