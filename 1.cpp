0bcfc66c305cd839705bf816002001e294ec0a8b
b6320502517 <sarannut.l@ku.th>
2021-03-01 19:00:54 +0700

    1

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,39 @@
 #include<stdio.h>
 int main(){
-	
+	int n=4,i,j,l,m=0,o=0;
+	if(n>3&&n<1000)
+		scanf("%d",&n);
+	int d[n][3]={},k[n]={};
+	for(i=0;i<n;i++)
+		for(j=0;j<3;j++)
+				scanf("%d",&d[i][j]);
+	for(i=0;i<n-2;i++){
+		for(j=0;j<3;j++){
+			if(j==0)
+				k[i]+=(d[i][j]*4)+(d[i+1][j]*4)+(d[i+2][j]*4);
+			else if (j==1)
+				k[i]+=(d[i][j]*2)+(d[i+1][j]*2)+(d[i+2][j]*2);
+			else if (j==2)
+				k[i]+=(d[i][j])+(d[i+1][j])+(d[i+2][j]);
+				
+		}
+	}
+	l=k[0];
+	for(i=0;i<n-2;i++){
+			if(l<k[i]){
+				l=k[i];
+				m=i;
+
+		}			
+	}
+	l=k[0];
+		for(i=0;i<n-2;i++){
+			if(l>k[i]){
+				l=k[i];
+				m=i;
+
+		}			
+	}
+	printf("%d %d",o+1,m+1);
+	return 0;
 }
