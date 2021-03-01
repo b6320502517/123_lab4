30198baacbb041ed5c00c93cd1c11b375b9a2d93
b6320502517 <sarannut.l@ku.th>
2021-03-01 17:48:56 +0700

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,7 +1,73 @@
 #include<stdio.h>
 int main(){
-	char x[50];
-	int n=1,i,j,k;
+	int n=1,i,j,k,l=1;
 	if(n>0&&n<100)
+		scanf("%d",&n);
+	char x[n][50];
+	for(i=0;i<n;i++){
+		scanf(" %[^\n]s",x[i]);
+	}
+	for(j=0;j<n;j++){
+		for(i=0;x[i]!='\0';i++){
+			if(x[j][i]>='A'&&x[j][i]<='E'){
+				if(x[j][i]=='A'){
+					if(l==1)
+						l=1;
+					else if(l==2)
+						l=3;
+					else if(l==3)
+						l=2;
+					else
+						l=4;
+				}
+				else if(x[j][i]=='B'){
+					if(l==1)
+						l=4;
+					else if(l==2)
+						l=2;
+					else if(l==3)
+						l=3;
+					else
+						l=1;
+				}
+				else if(x[j][i]=='C'){
+					if(l==1)
+						l=3;
+					else if(l==2)
+						l=4;
+					else if(l==3)
+						l=1;
+					else
+						l=2;
+				}
+				else if(x[j][i]=='D'){
+					if(l==1)
+						l=2;
+					else if(l==2)
+						l=1;
+					else if(l==3)
+						l=4;
+					else
+						l=3;
+				}
+				else if(x[j][i]=='E'){
+					if(l==1)
+						l=4;
+					else if(l==2)
+						l=3;
+					else if(l==3)
+						l=2;
+					else
+						l=1;
+				}
+
+		}
+		else
+			break;
+		
+		}
+		printf("%d\n",l);
+	}
+	
 	return 0;
 }
