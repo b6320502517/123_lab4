648e015de97a7b63fb851622ecf96deb795f41ae
b6320502517 <sarannut.l@ku.th>
2021-03-01 17:18:40 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -4,16 +4,18 @@ int main(){
 	char x[200] , y[200];
 	int i,j,k=0;
 	scanf("%[^\n]s",x);
-	for(i=0;i<strlen(x);i++){
+	for(i=0;x[i]!='\0';i++){
+		if(x[i]==' '){
+			break;
+		
+		}
 		if(x[i]!=x[i+1]){
 			y[k]=x[i];
 			k++;
-			printf("%c",x[i]);
-			//printf("%c\n",y[k]);
+			
 		}
 	}
-
-	//for(j=0;j<i;j++)
-		//printf("\n%c",y[j]);
+	
+	printf("%s",y);
 	return 0;
 }
